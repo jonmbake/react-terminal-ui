13e9b15526f70a4c127d8a6ae548a88860cb5de3
Tanmay Bhatt <tanmaybhatt.iet@gmail.com>
2024-04-03 00:45:12 +0530

    fix: removed hardcoded loader

--- demo/index.tsx
+++ demo/index.tsx
@@ -66,7 +66,6 @@ const TerminalController = (props = {}) => {
         redBtnCallback={ redBtnClick } 
         yellowBtnCallback={ yellowBtnClick } 
         greenBtnCallback={ greenBtnClick }
-        loading={true}
       >
         {lineData}
       </Terminal>
